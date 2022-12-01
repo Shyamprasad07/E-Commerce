75bfa5511e0d5c0241016bff5d5bd6139cc83293
SONU <srishyama01@gmail.com>
2022-12-01 19:47:45 +0530

    SplashScreen Ui is completed

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:emart_app/views/splash_screen.dart/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'consts/consts.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appname,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+        ),
+        fontFamily: regular,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/views/splash_screen.dart/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/applogo_widget.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: redColor,
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topLeft,
+                child: Image.asset(icSplashBg, width: 300)),
+            20.heightBox,
+            applogoWidget(),
+            10.heightBox,
+            appname.text.fontFamily(bold).size(22).white.make(),
+            5.heightBox,
+            appversion.text.white.make(),
+            const Spacer(),
+            credits.text.white.fontFamily(semibold).make(),
+            30.heightBox,
+          ],
+        ),
+      ),
+    );
+  }
+}
